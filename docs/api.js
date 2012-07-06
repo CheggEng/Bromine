7f6ad9c58ee8fffe682d60cf8be8658f9edde77b
Arieh Glazer <arieh.glazer@gmail.com>
2012-07-07 00:23:46 +0300

    added docs to pages

---
+++ docs/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bind",
+        "Bromine",
+        "Bromine.Test",
+        "Bromine.Tester",
+        "Events",
+        "Options"
+    ],
+    "modules": [
+        "Bromine",
+        "Bromine.utils",
+        "Events"
+    ],
+    "allModules": [
+        {
+            "displayName": "Bromine",
+            "name": "Bromine"
+        },
+        {
+            "displayName": "Bromine.utils",
+            "name": "Bromine.utils"
+        },
+        {
+            "displayName": "Events",
+            "name": "Events"
+        }
+    ]
+} };
+});
