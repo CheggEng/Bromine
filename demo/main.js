b09c654413e56172c7683135fedcbbb732c131cd
Arieh Glazer <arieh.glazer@gmail.com>
2012-07-07 13:49:31 +0300

    demo fix

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,6 @@
 //FX init 
 (function() {
-    var acord = new Fx.Accordion($('acord'), '#acord h2', '#acord p'),
+    var acord = new Fx.Accordion($('acord'), '#acord h2', '#acord .pane'),
         current = $$('.current');
 
     acord.addEvent('active', function(t, e) {
