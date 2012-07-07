87336684d4f3749fb66396bce9a346952dc2fca7
Arieh Glazer <arieh.glazer@gmail.com>
2012-07-07 11:30:40 +0300

    made sure license is also added to minified file

--- bin/bromine.js
+++ bin/bromine.js
@@ -1,3 +1,17 @@
+ /**!Copyright 2012 Chegg inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.   
+ */  
 !function(){
     /**
      * @module Events
@@ -429,6 +443,20 @@
     this.Events = Events;     
 }.call(this);
 
+/*!Copyright 2012 Chegg inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.   
+ */   
 !function(){
     /**
      * @module Bromine.utils
@@ -527,6 +555,20 @@
         utils: utils    
     };
 }.call(this);
+/**!Copyright 2012 Chegg inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.   
+ */
 !function(ns, utils){
     /**
      * @module Bromine
@@ -1049,6 +1091,20 @@
     Bromine.Tester = Tester;
     Bromine.Test = Test;
 }.apply(Bromine,[Bromine, Bromine.utils]);
+/*!Copyright 2012 Chegg inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.   
+ */
 !function(){
 
     function Reporter(){}
