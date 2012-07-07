52cdba6be8de2c03d22b7d58e90b2d3526b781c8
Arieh Glazer <arieh.glazer@gmail.com>
2012-07-07 10:42:40 +0300

    boundled reporter in

--- bin/bromine.js
+++ bin/bromine.js
@@ -1049,3 +1049,41 @@
     Bromine.Tester = Tester;
     Bromine.Test = Test;
 }.apply(Bromine,[Bromine, Bromine.utils]);
+!function(){
+
+    function Reporter(){}
+
+    Reporter.prototype = {
+        constructor : Reporter,
+        handleEvent : function(e){
+            var fn = this[e.type];
+
+            switch (e.type){
+                case 'done':
+                    fn(e.args.results);
+                    break;
+                case 'testStart':
+                    fn(e.args.name,e.args.test);
+                    break;
+                default:
+                    fn(e.args.name, e.args.results, e.args.test);
+            }
+        },
+        done : function(res, test){
+            console.log('Test Runner Done',[res,test]);
+        },
+        testStart : function(name, test){
+            console.log('Test Start',[name, res]);    
+        },
+        testDone : function(name, res, test){
+            console.log('Test Done',[name, res]);    
+        },
+        testFail : function(name, res, test){
+            console.log('Test Fail',[name, res]);    
+        }
+    };
+
+    Bromine.Reporter = Reporter;
+    
+}.call(Bromine);
+
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all:
 	touch bin/bromine.js
-	cat lib/Events/Events.js src/utils.js src/Bromine.js > bin/bromine.js
+	cat lib/Events/Events.js src/utils.js src/Bromine.js src/Reporter.js > bin/bromine.js
 	uglifyjs -o bin/bromine.min.js bin/bromine.js 
